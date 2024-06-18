d94c910f9c327f3ce865dbbab4196fef39633e99
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-18 14:56:57 +0300

    done

--- lib/teacher.py
+++ lib/teacher.py
@@ -4,7 +4,29 @@ from user import User
 
 import random
 
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
 class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=knowledge):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
+    
+# Create a new Teacher instance with the first name "John" and last name "Doe"
+teacher = Teacher("Daniel", "Beyene")
+
+# Access the teacher's first name, last name, and knowledge
+print(teacher.first_name)  
+print(teacher.last_name)   
+print(teacher.knowledge)   
